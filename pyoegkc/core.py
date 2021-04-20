0d6f15bc394a74dedc55c2fde8a1cc9bc8fd43f9
Simon Plakolb <s.plakolb@gmail.com>
2021-04-20 09:27:52 +0200

    Add converter class

---
+++ pyoegkc/core.py
@@ -0,0 +1,36 @@
+from enum import Enum
+
+from .data import load_district_merges,load_municipality_merges,load_municipality_splits
+
+class SplitStrategy(Enum):
+    """Strategy on how to proceed with splits.
+
+    Strategies
+    ----------
+    BIGGER_POPULOUS : Follow splits with more citizens involved.
+    LOWER_POPULOUS : Opposite of BIGGER_POPULOUS.
+    """
+
+    BIGGER_POPULOUS = 1
+    LOWER_POPULOUS = 2
+
+class Converter:
+    def __init__(self, strategy=SplitStrategy.BIGGER_POPULOUS):
+        self._dicts = [load_district_merges(), load_municipality_merges()]
+
+        splits = sorted(load_municipality_splits(), key=lambda x:x[3])
+
+        if strategy == SplitStrategy.LOWER_POPULOUS:
+            splits = reversed(splits)
+
+        self._dicts.append(
+                {pre:post for pre,post,_,_ in splits}
+                )
+
+    def convert(self, code):
+        for d in self._dicts:
+            if code in d:
+                return self.convert(self.d[code])
+        else:
+            return code
+
